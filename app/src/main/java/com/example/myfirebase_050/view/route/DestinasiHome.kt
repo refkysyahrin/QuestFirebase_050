786e59ed0380cda7bb75235bcf63034ba4241c73
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-02 21:42:31 +0700

    mendefinisikan objek DestinasiHome sebagai rute navigasi utama dengan judul aplikasi

--- app/src/main/java/com/example/myfirebase_050/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/myfirebase_050/view/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.myfirebase_050.view.route
 
-object DestinasiHome {
+import com.example.myfirebase_050.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
