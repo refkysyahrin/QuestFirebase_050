f063db9a7d47169ba0fa826abdd3c9209c338218
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-02 21:43:57 +0700

    implementasi EntryViewModel untuk menangani state form, validasi input, dan logika penyimpanan data siswa baru

--- app/src/main/java/com/example/myfirebase_050/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myfirebase_050/viewmodel/EntryViewModel.kt
@@ -1,4 +1,35 @@
 package com.example.myfirebase_050.viewmodel
 
-class EntryViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.myfirebase_050.DetailSiswa
+import com.example.myfirebase_050.RepositorySiswa
+import com.example.myfirebase_050.UIStateSiswa
+import com.example.myfirebase_050.toDataSiswa
+
+class EntryViewModel (private val repositorySiswa: RepositorySiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean
+    {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+    uiStateSiswa =
+        UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    //* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa(){
+        if (validasiInput()) {
+            repositorySiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+        }
+    }
 }
