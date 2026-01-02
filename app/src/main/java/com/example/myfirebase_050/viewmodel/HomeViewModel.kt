20de27470855592df4516afe6e2be92e4979c2b5
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-02 20:20:42 +0700

    implementasi HomeViewModel dengan manajemen state UI (StatusUiSiswa) dan integrasi fungsi load data dari repository

--- app/src/main/java/com/example/myfirebase_050/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/myfirebase_050/viewmodel/HomeViewModel.kt
@@ -1,2 +1,40 @@
 package com.example.myfirebase_050.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase_050.RepositorySiswa
+import com.example.myfirebase_050.Siswa
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
+    var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            statusUiSiswa = StatusUiSiswa.Loading
+            statusUiSiswa = try {
+                StatusUiSiswa.Success(repositorySiswa.getDataSiswa())
+            }catch (e: IOException) {
+                StatusUiSiswa.Error
+            }
+            catch (e: Exception) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
