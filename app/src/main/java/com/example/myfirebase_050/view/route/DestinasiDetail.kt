f3d8f1b25855d290d5fd8385fc7817faa36a448a
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-02 21:21:47 +0700

    mendefinisikan objek DestinasiDetail dengan rute navigasi dan parameter argumen idSiswa

--- app/src/main/java/com/example/myfirebase_050/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/myfirebase_050/view/route/DestinasiDetail.kt
@@ -1,4 +1,10 @@
 package com.example.myfirebase_050.view.route
 
-object DestinasiDetail {
+import com.example.myfirebase_050.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
